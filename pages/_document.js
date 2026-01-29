23ffb172a52b9880b533d3b4de8edf6ddc68f74d
Momen-Elshamy <eng.momen.adel@gmail.com>
2026-01-29 15:50:58 +0100

    fix: replace default Next.js favicon with PulsePal logo
    - Generated favicon.ico from pulsepal_logo.svg
    - Added apple-touch-icon.png for iOS
    - Added icon-192.png and icon-512.png for PWA support
    - Updated _document.js with proper favicon links

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon-192.png" type="image/png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
