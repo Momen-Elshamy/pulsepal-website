08b493c3db136376421b32a9b2a6fc508ccb55aa
Momen-Elshamy <eng.momen.adel@gmail.com>
2026-01-30 13:14:18 +0100

    feat: enable cross-subdomain cookie for tracking

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }) {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       person_profiles: 'identified_only',
       capture_pageview: false, // We'll capture manually for SPA
+      cross_subdomain_cookie: true, // Enable cross-domain tracking with ai.pulsepal.de
       loaded: (posthog) => {
         if (process.env.NODE_ENV === 'development') posthog.debug();
       }
